e771305a1b6fac75496e3407a6e1ee8d325c5534
RogerAirgood <rogerairgood@gmail.com>
2019-12-04 14:08:46 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+# Enable service
 sysrc -f /etc/rc.conf sabnzbd_enable="YES"
 
+# Permissions stuffs
 chown -R sabnzbd:sabnzbd /usr/local/share/sabnzbdplus
+chmod 755 /usr/local/sabnzbd/
 
-chmod 755 /usr/local/sabnzbdplus
-
+# Start service
 service sabnzbd start
