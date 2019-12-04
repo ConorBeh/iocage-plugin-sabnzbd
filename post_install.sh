84264c0bb19f8988a931ec692ca360a9b51e9f7c
RogerAirgood <rogerairgood@gmail.com>
2019-12-04 14:16:51 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -3,7 +3,10 @@
 sysrc -f /etc/rc.conf sabnzbd_enable="YES"
 
 # Permissions stuffs
-chown -R sabnzb:sabnzb /usr/local/share/sabnzbdplus
+pw user add sab -c sab -u 8675309 -d /nonexistent -s /usr/bin/nologin"
+pw groupadd -n sab -g 8675309"
+pw groupmod sab -m _sabnzbd"
+chown -R sab:sab /usr/local/share/sabnzbdplus
 chmod 755 /usr/local/sabnzbd/
 
 # Start service
