274e3a64ef0e7778b1d6da22de2f6db78558649b
RogerAirgood <rogerairgood@gmail.com>
2019-12-04 17:18:11 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -7,6 +7,7 @@ pw user add sab -c sab -u 8675309 -d /nonexistent -s /usr/bin/nologin
 pw groupadd -n sab -g 8675309
 pw groupmod sab -m _sabnzbd
 
+sed -i '' 's/127.0.0.1/0.0.0.0/g' /usr/local/sabnzbd/sabnzbd.ini
 
 # Start service
 service sabnzbd start
