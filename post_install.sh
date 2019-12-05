ba8aef2c3b961f349f31962b2eba51b92dfa244c
RogerAirgood <rogerairgood@gmail.com>
2019-12-04 17:26:32 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -3,9 +3,9 @@
 sysrc -f /etc/rc.conf sabnzbd_enable="YES"
 
 # Permissions stuffs
-pw user add sab -c sab -u 8675309 -d /nonexistent -s /usr/bin/nologin
-pw groupadd -n sab -g 8675309
-pw groupmod sab -m _sabnzbd
+#pw user add sab -c sab -u 8675309 -d /nonexistent -s /usr/bin/nologin
+#pw groupadd -n sab -g 8675309
+#pw groupmod sab -m _sabnzbd
 
 
 
@@ -14,7 +14,7 @@ service sabnzbd start
 
 sed -i '' 's/127.0.0.1/0.0.0.0/g' /usr/local/sabnzbd/sabnzbd.ini
 # More perms, needs to be done after service start
-chown -R sab:sab /usr/local/share/sabnzbdplus
+#chown -R sab:sab /usr/local/share/sabnzbdplus
 chmod 755 /usr/local/sabnzbd/
 
 service sabnzbd restart
