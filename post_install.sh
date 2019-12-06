1ee575d11bd84c5fd9eb9306b23afe1dc3f2b158
RogerAirgood <rogerairgood@gmail.com>
2019-12-06 15:12:47 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -17,7 +17,7 @@ sed -i '' 's/127.0.0.1/0.0.0.0/g' /usr/local/sabnzbd/sabnzbd.ini
 # More perms, needs to be done after service start
 #chown -R sab:sab /usr/local/share/sabnzbdplus
 # chmod -R 755 /usr/local/sabnzbd/
-service sabnzbd stop
+
 sleep 10
 service sabnzbd start
 exit
