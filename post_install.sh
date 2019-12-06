ff9f18bdfecab7437308b8e26cee4be32f3baf47
RogerAirgood <rogerairgood@gmail.com>
2019-12-06 15:23:14 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -13,8 +13,8 @@ sysrc sabnzbd_enable="YES"
 
 service sabnzbd start
 
-sed -i '' 's/127.0.0.1/0.0.0.0/g' /usr/local/sabnzbd/sabnzbd.ini
-chmod 777 /usr/local/sabnzbd/sabnzbd.ini
+#sed -i '' 's/127.0.0.1/0.0.0.0/g' /usr/local/sabnzbd/sabnzbd.ini
+#chmod 777 /usr/local/sabnzbd/sabnzbd.ini
 # More perms, needs to be done after service start
 #chown -R sab:sab /usr/local/share/sabnzbdplus
 # chmod -R 755 /usr/local/sabnzbd/
