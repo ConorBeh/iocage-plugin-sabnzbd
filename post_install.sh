1f1acb2a6dee09782c85f27376ad61770cd7d942
RogerAirgood <rogerairgood@gmail.com>
2020-01-15 16:16:10 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -3,10 +3,10 @@
 #sysrc sabnzbd_enable="YES"
 #service sabnzbd start 2>/dev/null
 #service sabnzbd stop 2>/dev/null
-service sabnzbd onestart
+service sabnzbd onestart 2>/dev/null
 sed -i '' 's/127.0.0.1/0.0.0.0/g' /usr/local/sabnzbd/sabnzbd.ini
 #chmod +x /usr/local/sabnzbd/sabnzbd.ini
-service sabnzbd onestop
+service sabnzbd onestop 2>/dev/null
 sysrc sabnzbd_enable="YES"
 service sabnzbd start 2>/dev/null
 
