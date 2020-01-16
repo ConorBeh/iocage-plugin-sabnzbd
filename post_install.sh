c4cd4680d54ee04e4943041fbf2f49ddfc002bf8
RogerAirgood <rogerairgood@gmail.com>
2020-01-15 16:05:46 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,6 +4,7 @@ sysrc sabnzbd_enable="YES"
 service sabnzbd start
 service sabnzbd stop
 sed -i '' 's/127.0.0.1/0.0.0.0/g' /usr/local/sabnzbd/sabnzbd.ini
+chmod +x /usr/local/sabnzbd/sabnzbd.ini
 #service sabnzbd stop
 service sabnzbd start
 
