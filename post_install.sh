6ad6e4f80be1e4defd0d5c2e57298cdfb9c53591
Martin Wilke <miwi@ixsystems.com>
2020-01-17 10:28:30 +0800

    - Cleanup

--- post_install.sh
+++ post_install.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 # Start/stop once to generate configs
-service sabnzbd onestart 
-sleep 10
-service sabnzbd onestop
+sysrc sabnzbd_enable="YES"
+service sabnzbd start 
 # Edit config to allow outside access
 sed -i '' 's/127.0.0.1/0.0.0.0/g' /usr/local/sabnzbd/sabnzbd.ini
-sleep 10
 # Start and enable service
-service sabnzbd onestart
-sysrc sabnzbd_enable="YES"
+service sabnzbd restart
 
 
