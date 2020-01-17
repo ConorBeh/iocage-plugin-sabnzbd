22901526778ce9346118d1c172455bcefa6d65ff
RogerAirgood <rogerairgood@gmail.com>
2020-01-16 19:33:33 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Start/stop once to generate configs
 service sabnzbd onestart 
-sleep 10
+#sleep 10
 service sabnzbd onestop
 # Edit config to allow outside access
 sed -i '' 's/127.0.0.1/0.0.0.0/g' /usr/local/sabnzbd/sabnzbd.ini
-sleep 10
+#sleep 10
 # Start and enable service
 service sabnzbd onestart
 sysrc sabnzbd_enable="YES"
