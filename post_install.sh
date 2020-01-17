787c23fc5401ae69ba6bb86cada62dd45ca21207
Martin Wilke <miwi@ixsystems.com>
2020-01-17 10:40:28 +0800

    - Restore sleep

--- post_install.sh
+++ post_install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # Start/stop once to generate configs
 sysrc sabnzbd_enable="YES"
-service sabnzbd prestart 
+service sabnzbd start 
+#Sleep is needed because iocage dont read line by line it reads all in post_install.sh
+sleep 5
 # Edit config to allow outside access
 sed -i '' 's/127.0.0.1/0.0.0.0/g' /usr/local/sabnzbd/sabnzbd.ini
 # Start and enable service
-service sabnzbd start
+service sabnzbd restart
 
 
