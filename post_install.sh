bded582b398b91bfc83fa722caa4f9ecca47a4dd
Martin Wilke <miwi@ixsystems.com>
2020-01-17 10:34:21 +0800

    - Use Prestart

--- post_install.sh
+++ post_install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Start/stop once to generate configs
 sysrc sabnzbd_enable="YES"
-service sabnzbd start 
+service sabnzbd prestart 
 # Edit config to allow outside access
 sed -i '' 's/127.0.0.1/0.0.0.0/g' /usr/local/sabnzbd/sabnzbd.ini
 # Start and enable service
-service sabnzbd restart
+service sabnzbd start
 
 
