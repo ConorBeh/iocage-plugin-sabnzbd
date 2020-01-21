11f32485bf13da456fb85702429648a505045884
ConorBeh <rogerairgood@gmail.com>
2020-01-20 22:52:33 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -2,11 +2,11 @@
 # Enable and start the service to generate the config file
 sysrc sabnzbd_enable="YES"
 service sabnzbd start 
-# Sleep is needed because iocage doesn’t read line by line but rather reads it all at once
+# Sleep is needed because iocage doesn’t read line by line but rather reads it in all at once
 sleep 5
 # Edit config to allow outside access
 sed -i '' 's/127.0.0.1/0.0.0.0/g' /usr/local/sabnzbd/sabnzbd.ini
-# Restart service for config change to take 
+# Restart the service for config change to take 
 service sabnzbd restart
 
 
